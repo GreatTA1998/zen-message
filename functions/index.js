69d7a626d6822b880fd2dc2fb69d8663a334c492
Elton Lin <eltonlin1998@gmail.com>
2022-08-29 17:58:56 -0700

    .

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const { initializeApp } = require('firebase-admin/app');
+const adminCredentials = require("./adminCredentials.json");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -26,3 +28,43 @@ exports.sendTextMessage = functions.https.onCall((data, context) => {
     .then(message => console.log('sent to =', toWho)) 
     .done();
 });
+
+
+// test local cloud environment
+// try `every 24 hours`
+exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
+  console.log('This will be run every 5 minutes!');
+
+  const firestore = admin.firestore()
+  const usersRef = firestore.collection('users')
+  const result = await usersRef.get()
+  if (result.exists) {
+      result.docs.forEach(userDoc => {
+          const user = userDoc.data()
+          let summaryOfMessages = []
+          for (const friendUID of user.friendUIDsWithNewMessages) {
+            const chatID = user.uid < friendUID ? (user.uid + friendUID) : (friendUID + user.uid)
+            const chatRef = firestore.doc(`chats/${chatID}`)
+            const chatResult = await chatRef.get() 
+            const chatObj = chatResult.data()
+            const mostRecentMessage = chatObj.messages[chatObj.messages.length - 1]
+            summaryOfMessages.push(mostRecentMessage)
+          }
+          // send a text
+          // to use the function we already have https://stackoverflow.com/a/60470745/7812829
+            client.messages 
+            .create({        
+                body: summaryOfMessages.join(''),
+                messagingServiceSid: 'MGe595615f66055b9ee88ac19a9d0ddce5',
+                to: user.phoneNumber
+              }) 
+            .then(message => console.log('sent to =', user.phoneNumber)) 
+            .done();
+      })
+  }
+  return null;
+})
+
+
+
+
